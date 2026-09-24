f7ce75d2653f128cc0502ff9cbe8601f946b12a1
Kinan Athar <geometryv31l.gd@gmail.com>
2026-09-24 09:58:06 +0530

    Update and rename v31lCalculator to Very_simple_Calculator.py
    Just a newbie calculator

---
+++ Very_simple_Calculator.py
@@ -0,0 +1,25 @@
+num1 = int(input("Enter your number A here: "))
+num2 = int(input("Enter your number B here: "))
+op = input("Enter what type of operation would you want to do: ") 
+
+if op == "+":
+  print(num1 + num2)
+
+if op == "-":
+  print(num1 - num2)
+
+if op == "*":
+  print(num1 * num2)
+
+if op == "/":
+  print(num1 / num2)
+
+if op == "%":
+  print(num1 % num2)
+
+if op == "**":
+  print(num1 ** num2)
+
+print("end of code")
+
+#Guys if you come across this, this is my first ever self-written code. Thank you for visiting!
